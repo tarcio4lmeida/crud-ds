8baf7f03802266565e476a88051ca571aa73eeac
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-10-04 12:41:37 -0300

    Project created

---
+++ backend/src/main/java/com/apicrudds/backend/ApiCrudDsApplication.java
@@ -0,0 +1,13 @@
+package com.apicrudds.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudDsApplication.class, args);
+	}
+
+}
