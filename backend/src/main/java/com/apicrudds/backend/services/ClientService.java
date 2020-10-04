43119abb351cf08e0b78532aaaf24592ffc7ae49
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-10-04 13:37:03 -0300

    Seeding DataBase

--- backend/src/main/java/com/apicrudds/backend/services/ClientService.java
+++ backend/src/main/java/com/apicrudds/backend/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.apicrudds.backend.entities.Client;
 import com.apicrudds.backend.resources.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired
 	ClientRepository clientRepository;
 	
+	@Transactional(readOnly=true)
 	public List<Client> findAll(){
 		return clientRepository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+insert into tb_client(name,cpf,birthDate,income,children) values('Giovana Daiane Clara da Luz', '62078822108', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',6500.0, 2);
+insert into tb_client(name,cpf,birthDate,income,children) values('Liz Larissa Moreira', '71002259908', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',7100.0, 3);
+insert into tb_client(name,cpf,birthDate,income,children) values('José Pedro Jesus', '23521254488', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 14500.0, 6);
+insert into tb_client(name,cpf,birthDate,income,children) values('Carolina Beatriz Manuela Araújo', '23715149183', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 20500.0, 2);
+insert into tb_client(name,cpf,birthDate,income,children) values('Kauê Pedro Diogo da Luz', '25869315212', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',4500.0, 0);
+insert into tb_client(name,cpf,birthDate,income,children) values('Marlene Hadassa dos Santos', '09407912779', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',6500.0, 2);
+insert into tb_client(name,cpf,birthDate,income,children) values('Clara Alessandra Carvalho', '62556306688', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 5500.0, 1);
+insert into tb_client(name,cpf,birthDate,income,children) values('Joana Kamilly da Conceição', '46355244930', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 10000.0, 3);
+insert into tb_client(name,cpf,birthDate,income,children) values('José Pedro Oliveira', '23521254488', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 12500.0, 4);
+insert into tb_client(name,cpf,birthDate,income,children) values('Tarcio Almeida Lima', '68548752678', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 9000.0, 0);
+
+
